7fefbca07ab99a25ce81e50a904d991d773c4451
VinayakThorat21 <vmt0510offi@gmail.com>
2024-11-27 08:18:33 +0530

    Initial commit

---
+++ aad/auth.js
@@ -0,0 +1,50 @@
+//MSAL configuration
+const msalConfig = {
+    auth: {
+        clientId: 'eecd27f7-5272-4553-872c-9f0ddfaa4ce1',
+        // comment out if you use a multi-tenant AAD app
+        authority: 'https://login.microsoftonline.com/common',
+        redirectUri: 'https://3362-117-239-185-165.ngrok-free.app'
+    }
+};
+const msalRequest = { scopes: [] };
+function ensureScope (scope) {
+    if (!msalRequest.scopes.some((s) => s.toLowerCase() === scope.toLowerCase())) {
+        msalRequest.scopes.push(scope);
+    }
+}
+//Initialize MSAL client
+const msalClient = new msal.PublicClientApplication(msalConfig);
+
+// Log the user in
+async function signIn() {
+    const authResult = await msalClient.loginPopup(msalRequest);
+    sessionStorage.setItem('msalAccount', authResult.account.username);
+}
+//Get token from Graph
+async function getToken() {
+    let account = sessionStorage.getItem('msalAccount');
+    if (!account) {
+        throw new Error(
+            'User info cleared from session. Please sign out and sign in again.');
+    }
+    try {
+        // First, attempt to get the token silently
+        const silentRequest = {
+            scopes: msalRequest.scopes,
+            account: msalClient.getAccountByUsername(account)
+        };
+
+        const silentResult = await msalClient.acquireTokenSilent(silentRequest);
+        return silentResult.accessToken;
+    } catch (silentError) {
+        // If silent requests fails with InteractionRequiredAuthError,
+        // attempt to get the token interactively
+        if (silentError instanceof msal.InteractionRequiredAuthError) {
+            const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
+            return interactiveResult.accessToken;
+        } else {
+            throw silentError;
+        }
+    }
+}
